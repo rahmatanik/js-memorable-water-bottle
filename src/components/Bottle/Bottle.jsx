05cbd6ed35b19485dccc168f5f704e2861286176
Rahmat Ullah <rahmat.anik7@gmail.com>
2023-09-12 21:18:37 +0600

    completed

---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import "./Bottle.css";
+
+const Bottle = ({ bottle, handleAddToCart }) => {
+  const { name, img, price } = bottle;
+  return (
+    <div className="bottle">
+      <h3>Bottle : {name}</h3>
+      <img src={img} alt="" />
+      <p>Price: ${price}</p>
+      <button onClick={() => handleAddToCart(bottle)}>Add to Cart</button>
+    </div>
+  );
+};
+
+Bottle.propTypes = {
+  bottle: PropTypes.object.isRequired,
+  handleAddToCart: PropTypes.func.isRequired,
+};
+export default Bottle;
